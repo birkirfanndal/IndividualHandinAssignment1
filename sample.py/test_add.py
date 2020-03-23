c0ee1c4bf4a0d46dc64141bbf6bc88e8bace4cc2
Birkir Fanndal <birkirfanndal1999@gmail.com>
2020-03-23 23:16:20 +0000

    step 6 fix2

--- sample.py/test_add.py
+++ sample.py/test_add.py
@@ -6,4 +6,4 @@ def add_test():
     assert add.Add("1,2") == 3
     assert add.Add("1,2,3,4") == 10
     assert add.Add("21,5\n4,7") == 37
-    assert add.Add("2,59\n1004" == 61
+    assert add.Add("2,59\n1004") == 61
