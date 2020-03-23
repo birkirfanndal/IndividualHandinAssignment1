253d1f08693719f73b48a236ee910f4ad4bf9554
Birkir Fanndal <birkirfanndal1999@gmail.com>
2020-03-23 23:12:32 +0000

    step 5

--- sample.py/add.py
+++ sample.py/add.py
@@ -1,11 +1,15 @@
 
+import re
+
 def add(numbers):
     if numbers == "":
         return "0"
+
     elif "," not in numbers:
         return str(numbers)
+
     elif "," in numbers:
-        num_list = numbers.split(",")
+        num_list = re.split(",|\n", numbers)
         num_sum = 0
         for i in num_list:
             num_sum += int(i)
--- sample.py/test_add.py
+++ sample.py/test_add.py
@@ -5,3 +5,4 @@ def add_test():
     assert add.Add("1") == 1
     assert add.Add("1,2") == 3
     assert add.Add("1,2,3,4") == 10
+    assert add.Add("21,5\n4,7") == 37
