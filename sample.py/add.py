8870881e8083d4fa418a63cf74769529543b7519
Birkir Fanndal <birkirfanndal1999@gmail.com>
2020-03-23 22:54:14 +0000

    step 3

--- sample.py/add.py
+++ sample.py/add.py
@@ -2,5 +2,9 @@
 def add(numbers):
     if numbers == "":
         return "0"
-    else:
+    elif "," not in numbers:
         return str(numbers)
+    else:
+        num_list = numbers.split(",")
+        num_sum = int(num_list[0]) + int(num_list[1])
+        return str(num_sum)
--- sample.py/test_add.py
+++ sample.py/test_add.py
@@ -3,3 +3,4 @@ from add import add
 def add_test():
     assert add.Add("") == 0
     assert add.Add("1") == 1
+    assert add.Add("1,2") == 3
